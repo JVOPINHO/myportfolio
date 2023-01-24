512bb0d326898c1ad83c8ee236f6ede729a0ab14
jvopinho <joaoamoreterno1511@gmail.com>
2023-01-24 02:26:06 +0000

    Lint

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import styles from './styles.module.scss';
 
 export const Header: React.FC = () => (
-    <header className={styles.wrapper}>
-        <div className={styles.header}>
-            <h4>Hellow I&apos;m</h4>
-            <h1>João Pinho</h1>
-            <br />
-            <h5>Backend Developer</h5>
-        </div>
-    </header>
+	<header className={styles.wrapper}>
+		<div className={styles.header}>
+			<h4>Hellow I&apos;m</h4>
+			<h1>João Pinho</h1>
+			<br />
+			<h5>Backend Developer</h5>
+		</div>
+	</header>
 );
--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -5,12 +5,12 @@ import styles from './styles.module.scss';
 import { skills as _skills } from '../../config';
 
 const skills = [
-    ..._skills.filter(skill => skill.favorite),
-    ..._skills.filter(skill => !skill.favorite),
-]
+	..._skills.filter(skill => skill.favorite),
+	..._skills.filter(skill => !skill.favorite),
+];
 
 export const Skills: React.FC = () => (
-    <div className={styles.skills} style={{gridTemplateColumns: `repeat(${skills.length <= 10 ? 10 : skills.length}, 1fr)`}}>
-        {skills.map(skill => (<img key={skill.name} src={skill.image} />))}
-    </div>
+	<div className={styles.skills} style={{gridTemplateColumns: `repeat(${skills.length <= 10 ? 10 : skills.length}, 1fr)`}}>
+		{skills.map(skill => (<img key={skill.name} src={skill.image} />))}
+	</div>
 );
