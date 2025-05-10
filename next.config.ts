d4a63ee401c1cb4320d9f83f1bdbd12faf20b6e3
jvopinho <jvopinho@isluny.org>
2025-05-10 20:16:54 -0300

    fix: styled-components

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   sassOptions: {
 		includePaths: [path.join(__dirname, 'styles')],
 	},
+  compiler: {
+    styledComponents: true
+  }
 };
 
 export default nextConfig;
