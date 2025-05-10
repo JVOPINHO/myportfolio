a11a2ae9f9fa5b5ee03ffea487c9b3b79498b67e
jvopinho <jvopinho@isluny.org>
2025-05-10 20:06:26 -0300

    fix: sassOptions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+		includePaths: [__dirname],
+	},
 };
 
 export default nextConfig;
